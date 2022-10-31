35027db9ba7d1fd1bf0ef290ff5e4b3d8c99c5e0
Bealby <mgbealby@gmail.com>
2022-10-31 21:39:40 +0100

    Fixed about page for nav

--- ignite_theatre/urls.py
+++ ignite_theatre/urls.py
@@ -19,7 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+        path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('about/', include('about.urls')),
+    path('gallery/', include('gallery.urls')),
+    path('cast/', include('cast.urls')),
+    path('tickets/', include('tickets.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
