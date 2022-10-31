9f977f7d6ccffc8ccf302e50f3799cb347d1fa35
Bealby <mgbealby@gmail.com>
2022-10-31 21:33:59 +0100

    Updated ADMIN

--- cast/admin.py
+++ cast/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Show
+from .models import Actor
+from .models import Crew
+from .models import Cast
 
-# Register your models here.
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'poster',
+    )
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = (
+        'show',
+        'name',
+        'image',
+        'description',
+        'website',
+        'position',
+    )
+
+    ordering = ('position',)
+
+class CrewAdmin(admin.ModelAdmin):
+    list_display = (
+        'show',
+        'name',
+        'role',
+    )
+
+class CastAdmin(admin.ModelAdmin):
+    list_display = (
+        'show',
+        'name',
+        'role',
+    )
+
+
+admin.site.register(Show, ShowAdmin)
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Crew, CrewAdmin)
+admin.site.register(Cast, CastAdmin)
--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Show
+from .models import Ticket
 
-# Register your models here.
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'poster',
+    )
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = (
+        'show',
+        'name',
+        'event_date',
+        'place',
+        'location',
+        'image',
+        'position',
+        'description',
+        'event_details',
+        'price_details',
+        'date',
+        'price',       
+    )
+
+    ordering = ('position',)
+
+admin.site.register(Show, ShowAdmin)
+admin.site.register(Ticket, TicketAdmin)
