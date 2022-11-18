bdcb59a55706c5890f73094ad9ce674ea7bd6c88
Bealby <mgbealby@gmail.com>
2022-11-18 19:44:48 +0100

    Set-up contact form functionality

--- contact/views.py
+++ contact/views.py
@@ -10,16 +10,16 @@ def contactView(request):
     else:
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data["subject"]
-            from_email = form.cleaned_data["from_email"]
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ["admin@example.com"])
+                send_mail(name, message, email, ["admin@example.com"])
             except BadHeaderError:
                 return HttpResponse("Invalid header found.")
             return redirect("success")
-    return render(request, "contact.html", {"form": form})
+    return render(request, "contact/contact.html", {"form": form})
 
 
 def successView(request):
-    return HttpResponse("Success! Thank you for your message.")
+    return render(request, "contact/contact_success.html")
