cf85495c54aee8bd3f0d77514cac72acf87d0bbe
Bealby <mgbealby@gmail.com>
2022-11-26 11:50:27 +0100

    Added upcoming model. Added views and links to template

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from .models import Show
+from .models import Upcoming
 
 
 def index(request):
     ''' A view to return index page'''
 
     shows = Show.objects.all()
+    upcomings = Upcoming.objects.all()
 
     context = {
         'shows': shows,
+        'upcomings': upcomings,
     }
 
     return render(request, 'home/index.html', context)
