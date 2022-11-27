01650e495be29af6b2de8ac8968833d0f03dbe7b
Bealby <mgbealby@gmail.com>
2022-11-27 12:36:00 +0100

    Revert forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -2,24 +2,24 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
+    name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
 
     # Fields in contact form
     class Meta:
-        fields = ['from_email', 'subject', 'message']
+        fields = ['name', 'email', 'message']
 
     # Placeholders in contact form
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         placeholders = {
-            'from_email': 'Email',
-            'subject': 'Subject',
+            'name': 'Full Name',
+            'email': 'Email',
             'message': 'How can we help?',
         }
 
-        self.fields['from_email'].widget.attrs['autofocus'] = True
+        self.fields['name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]}'
