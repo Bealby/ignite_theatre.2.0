b4cd0432c93813594bcc27f027f0babdcf67b8b7
Bealby <mgbealby@gmail.com>
2022-11-28 10:45:53 +0100

    Remove body bg

--- ignite_theatre/settings.py
+++ ignite_theatre/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ignite-theatre.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ignite-theatre.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
