8cd546f8b4cec766ae6dc25b5ee0fc1b495351e5
Bealby <mgbealby@gmail.com>
2022-11-29 23:43:41 +0100

    Add new allowed hosts links

--- ignite_theatre/settings.py
+++ ignite_theatre/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ignite-theatre.herokuapp.com', 'localhost', '127.0.0.1', 'www.ignite-theatre.com']
+ALLOWED_HOSTS = ['ignite-theatre.herokuapp.com', 'localhost', '127.0.0.1',
+                 'www.ignite-theatre.com', 'ignite-theatre.com']
 
 
 # Application definition
