c355f79b738639fd2d7d341dd82e4bbfd175c300
Bealby <mgbealby@gmail.com>
2025-01-31 20:09:57 +0100

    Deploy

--- ignite_theatre/settings.py
+++ ignite_theatre/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    #'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'ignite_theatre.urls'
