34ec5836e8e3f4112b1adcdc98a2a5cdba01c815
Bealby <mgbealby@gmail.com>
2025-01-31 20:03:45 +0100

    Deploy

--- ignite_theatre/settings.py
+++ ignite_theatre/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     # Other
     'crispy_forms',
     'storages',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'ignite_theatre.urls'
