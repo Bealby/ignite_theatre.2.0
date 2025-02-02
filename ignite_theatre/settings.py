7920e2ac5e044f14383d5bbb9950e4f62151139e
Bealby <mgbealby@gmail.com>
2025-02-02 06:28:44 +0100

    Updated/ Set-up carousel for all plays on website

--- ignite_theatre/settings.py
+++ ignite_theatre/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     # Other
     'crispy_forms',
     'storages',
-    'crispy_bootstrap4',
+    #'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    #'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'ignite_theatre.urls'
