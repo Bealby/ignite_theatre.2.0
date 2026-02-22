c89feb4214f39d8d3c823775dc0864d0862a3a59
Bealby <mgbealby@gmail.com>
2026-02-22 20:30:25 +0100

    Temporarily enable debug

--- ignite_theatre/settings.py
+++ ignite_theatre/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ignite-theatre.herokuapp.com', 'localhost', '127.0.0.1',
                  'www.ignite-theatre.com', 'ignite-theatre.com',
