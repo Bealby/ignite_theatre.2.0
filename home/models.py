1b7e1cefa832e11ca2880094a8d9799b4f00fb08
Bealby <mgbealby@gmail.com>
2026-02-22 20:11:43 +0100

    Fix Home Page

--- home/models.py
+++ home/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class Meeching(models.Model):
     '''Programmatic Name'''
     name = models.CharField(max_length=254, null=True, blank=True)
-    meeching_text = models.TextField(max_length=400, null=True, blank=True)
-    meeching_poster = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
