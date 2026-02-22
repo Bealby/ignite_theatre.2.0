25b8a791acc51fbba53a4b6cacb6f0ca571b1a75
Bealby <mgbealby@gmail.com>
2026-02-22 19:47:58 +0100

    Edit Home page

--- home/admin.py
+++ home/admin.py
@@ -1,34 +1,44 @@
 from django.contrib import admin
-from .models import Show
-from .models import Upcoming
+# from .models import Show
+# from .models import Upcoming
+from .models import MeechingHome
 
 
-class ShowAdmin(admin.ModelAdmin):
+class MeechingHomeAdmin(admin.ModelAdmin):
     list_display = (
         'name',
-        'date',
-        'ticket_retail_image',
-        'ticket_retail_name',
-        'ticket_retail_url',
-        'ticket_retail_address',
-        'show_poster',
-        'show_sound_bite',
-        'show_content',
+        'meeching_text',
+        'meeching_poster',
     )
 
 
-class UpcomingAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'location',
-        'date',
-        'location_2',
-        'date_2',
-        'location_3',
-        'date_3',
-        'position',
-    )
+# class ShowAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'name',
+#         'date',
+#         'ticket_retail_image',
+#         'ticket_retail_name',
+#         'ticket_retail_url',
+#         'ticket_retail_address',
+#         'show_poster',
+#         'show_sound_bite',
+#         'show_content',
+#     )
 
 
-admin.site.register(Show, ShowAdmin)
-admin.site.register(Upcoming, UpcomingAdmin)
+# class UpcomingAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'name',
+#         'location',
+#         'date',
+#         'location_2',
+#         'date_2',
+#         'location_3',
+#         'date_3',
+#         'position',
+#     )
+
+
+# admin.site.register(Show, ShowAdmin)
+# admin.site.register(Upcoming, UpcomingAdmin)
+admin.site.register(MeechingHome, MeechingHomeAdmin)
--- home/views.py
+++ home/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
-from .models import Show
-from .models import Upcoming
+# from .models import Show
+# from .models import Upcoming
+from .models import MeechingHome
 
 
 def index(request):
     ''' A view to return index page'''
 
-    shows = Show.objects.all()
-    upcomings = Upcoming.objects.all()
+    # shows = Show.objects.all()
+    # upcomings = Upcoming.objects.all()
+    meechings = MeechingHome.objects.all()
 
     context = {
-        'shows': shows,
-        'upcomings': upcomings,
+        # 'shows': shows,
+        # 'upcomings': upcomings,
+        'meechings': meechings,
     }
 
     return render(request, 'home/index.html', context)
