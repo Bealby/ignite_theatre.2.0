607f755cc8f5fc6161fc9276d5c12f3f6a029af1
Bealby <mgbealby@gmail.com>
2026-02-22 19:57:54 +0100

    Fix

--- home/models.py
+++ home/models.py
@@ -2,43 +2,34 @@ from django.db import models
 '''Pillow must be installed'''
 
 
-class MeechingHome(models.Model):
+class Show(models.Model):
     '''Programmatic Name'''
     name = models.CharField(max_length=254, null=True, blank=True)
-    meeching_text = models.TextField(max_length=400, null=True, blank=True)
-    meeching_poster = models.ImageField(null=True, blank=True)
+    date = models.CharField(max_length=254, null=True, blank=True)
+    ticket_retail_image = models.ImageField(null=True, blank=True)
+    ticket_retail_name = models.CharField(max_length=254, null=True,
+                                          blank=True)
+    ticket_retail_url = models.CharField(max_length=254, null=True, blank=True)
+    ticket_retail_address = models.TextField(max_length=400, null=True,
+                                             blank=True)
+    show_poster = models.ImageField(null=True, blank=True)
+    show_sound_bite = models.TextField(max_length=1000, null=True, blank=True)
+    show_content = models.TextField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-# class Show(models.Model):
-#     '''Programmatic Name'''
-#     name = models.CharField(max_length=254, null=True, blank=True)
-#     date = models.CharField(max_length=254, null=True, blank=True)
-#     ticket_retail_image = models.ImageField(null=True, blank=True)
-#     ticket_retail_name = models.CharField(max_length=254, null=True,
-#                                           blank=True)
-#     ticket_retail_url = models.CharField(max_length=254, null=True, blank=True)
-#     ticket_retail_address = models.TextField(max_length=400, null=True,
-#                                              blank=True)
-#     show_poster = models.ImageField(null=True, blank=True)
-#     show_sound_bite = models.TextField(max_length=1000, null=True, blank=True)
-#     show_content = models.TextField(max_length=1000, null=True, blank=True)
 
-#     def __str__(self):
-#         return self.name
+class Upcoming(models.Model):
+    '''Programmatic Name'''
+    name = models.CharField(max_length=254)
+    location = models.CharField(max_length=254, null=True, blank=True)
+    date = models.CharField(max_length=254, null=True, blank=True)
+    location_2 = models.CharField(max_length=254, null=True, blank=True)
+    date_2 = models.CharField(max_length=254, null=True, blank=True)
+    location_3 = models.CharField(max_length=254, null=True, blank=True)
+    date_3 = models.CharField(max_length=254, null=True, blank=True)
+    position = models.CharField(max_length=254, null=True, blank=True)
 
-
-# class Upcoming(models.Model):
-#     '''Programmatic Name'''
-#     name = models.CharField(max_length=254)
-#     location = models.CharField(max_length=254, null=True, blank=True)
-#     date = models.CharField(max_length=254, null=True, blank=True)
-#     location_2 = models.CharField(max_length=254, null=True, blank=True)
-#     date_2 = models.CharField(max_length=254, null=True, blank=True)
-#     location_3 = models.CharField(max_length=254, null=True, blank=True)
-#     date_3 = models.CharField(max_length=254, null=True, blank=True)
-#     position = models.CharField(max_length=254, null=True, blank=True)
-
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
-# from .models import Show
-# from .models import Upcoming
-from .models import MeechingHome
+from .models import Show
+from .models import Upcoming
 
 
 def index(request):
     ''' A view to return index page'''
 
-    # shows = Show.objects.all()
-    # upcomings = Upcoming.objects.all()
-    meechings = MeechingHome.objects.all()
+    shows = Show.objects.all()
+    upcomings = Upcoming.objects.all()
 
     context = {
-        # 'shows': shows,
-        # 'upcomings': upcomings,
-        'meechings': meechings,
+        'shows': shows,
+        'upcomings': upcomings,
     }
 
     return render(request, 'home/index.html', context)
